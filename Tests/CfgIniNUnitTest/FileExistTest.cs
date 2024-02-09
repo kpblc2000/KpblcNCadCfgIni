ae5b2e640ca2606c11b21febcca6cf43c50385bc
kpblc2000 <kpblc2000@gmail.com>
2024-02-10 00:36:17 +0300

    Первый работающий тест

---
+++ Tests/CfgIniNUnitTest/FileExistTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System.Linq;
+
+namespace CfgIniNUnitTest
+{
+    public class FileExistTest
+    {
+        [Test, Order(0)]
+        public void CheckFileExistTest()
+        {
+            Assert.IsTrue(File.Exists(TestSetup.ConfigFileName));
+        }
+    }
+}
