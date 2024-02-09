8fabce3157ebbf99a7608569e71cc275201201b7
kpblc2000 <kpblc2000@gmail.com>
2024-02-10 00:23:08 +0300

    Добавление проекта для NUnit-тестирования. Создание класса для получения пути к cfg.ini

---
+++ Tests/CfgIniNUnitTest/TestSetup.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace CfgIniNUnitTest
+{
+    public static class TestSetup
+    {
+        public static string ConfigFileName => Path.Combine(Path.GetDirectoryName(typeof(TestSetup).Assembly.Location), "cfg.ini");
+    }
+}
